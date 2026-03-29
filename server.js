98bb73375e1457bfa1c9749e1b4170d58ac1e702
Mann Prajapati <mannprajapati2502@gmail.com>
2026-03-29 17:32:51 +0530

    server.js updated

--- server.js
+++ server.js
@@ -73,8 +73,13 @@ app.get('/', (req, res) => {
 module.exports = app;
 
 // Local development listener
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     app.listen(port, () => {
+//         console.log(`Server running at http://localhost:${port}`);
+//     });
+// }
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
